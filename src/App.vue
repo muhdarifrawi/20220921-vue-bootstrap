5575cafcaf2663b7aab0b803e068d0e25908b71c
arif <muhdarifrawi@gmail.com>
2022-09-21 15:18:30 +0800

    initial commit.

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import Landing from "./components/Landing.vue"
+</script>
+
+<template>
+  <Landing/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// removed main.css
+// import "./assets/main.css";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
